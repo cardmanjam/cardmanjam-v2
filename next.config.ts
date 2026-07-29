f864dec21c7e5327bdec476ad818cb9910e4eff8
cardmanjam <96jamroz@gmail.com>
2026-07-29 18:53:59 -0400

    Increase product photo upload limit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "**.supabase.co" }
-    ]
-  }
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "25mb",
+    },
+  },
 };
+
 export default nextConfig;
